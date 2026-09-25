d9eb9b9907ea6d6a465c5ea6cc27935a6ff93025
XiqZo <waleed-4-ever@hotmail.com>
2026-09-25 11:36:46 +0200

    add reeling hints

--- source/Scenes/FishingScene.lua
+++ source/Scenes/FishingScene.lua
@@ -1,23 +1,32 @@
 local pd <const> = playdate
 local gfx <const> = pd.graphics
+
 local menu = pd.getSystemMenu()
 
 FishingScene = {}
+
 class("FishingScene").extends(gfx.sprite)
 
 local STATES <const> = {
+
     casting = "casting",
+
     chargingCast = "chargingCast",
+
     reeling = "reeling",
+
     fightingFish = "fightingFish"
+
 }
 
 function FishingScene:init()
+
     self.player = Player()
     self.rod = FishingRod()
     self.fish = Fish()
 
     self.state = STATES.casting
+
     self.time = 0
 
     self.reelProgress = 0
@@ -31,6 +40,9 @@ function FishingScene:init()
     self.escapeTimer = 0
     self.escapeTime = 5
 
+    -- Current reel feedback
+    self.reelFeedback = ""
+
     self.shore = gfx.sprite.new(
         gfx.image.new(1, 240, gfx.kColorBlack)
     )
@@ -40,6 +52,7 @@ function FishingScene:init()
     self.shore:add()
 
     self.overlay = gfx.sprite.new()
+
     self.overlay:setBounds(0, 0, 400, 240)
     self.overlay:setZIndex(100)
     self.overlay:setIgnoresDrawOffset(true)
@@ -56,60 +69,81 @@ function FishingScene:init()
     )
 
     self.overlay:add()
+
     self:add()
+
 end
 
 function FishingScene:update()
+
     self.time += 1 / 30
 
     local change, acceleratedChange =
         pd.getCrankChange()
 
     if self.state == STATES.casting then
+
         self:castingUpdate()
 
     elseif self.state == STATES.chargingCast then
+
         self:chargingUpdate()
 
     elseif self.state == STATES.reeling then
+
         self:reelingUpdate(
             change,
             acceleratedChange
         )
 
     elseif self.state == STATES.fightingFish then
+
         self:fightingFishUpdate(
             change,
             acceleratedChange
         )
+
     end
 
     self.overlay:markDirty()
+
 end
 
 function FishingScene:castingUpdate()
+
     self.player:move("ws")
+
     self.fish:update(self.time)
 
     if pd.buttonJustPressed(pd.kButtonA) then
+
         self.rod.castPower = 0
+
         self.state = STATES.chargingCast
+
     end
+
 end
 
 function FishingScene:chargingUpdate()
+
     self.rod:updateCastPower()
 
     if pd.buttonJustReleased(pd.kButtonA) then
+
         self.rod:cast(self.player)
+
         self.state = STATES.reeling
+
     end
+
 end
 
 function FishingScene:reelingUpdate(
     change,
     acceleratedChange
 )
+
     self.rod:update(
         change,
         acceleratedChange
@@ -122,24 +156,49 @@ function FishingScene:reelingUpdate(
         hookX,
         hookY
     ) then
+
         self.reelProgress = 0
         self.escapeTimer = 0
+        self.reelFeedback = "REEL!"
+
         self.state = STATES.fightingFish
 
     elseif self.rod:isFinished() then
+
         self.state = STATES.casting
+
     end
+
 end
 
 function FishingScene:fightingFishUpdate(
     change,
     acceleratedChange
 )
-    local reelSpeed = math.abs(change * acceleratedChange)
 
+    local reelSpeed =
+        math.abs(change * acceleratedChange)
+
+    -- Determine reel speed feedback
+    if reelSpeed < self.safeMinReelSpeed then
+
+        self.reelFeedback = "TOO SLOW"
+
+    elseif reelSpeed > self.safeMaxReelSpeed then
+
+        self.reelFeedback = "TOO FAST"
+
+    else
+
+        self.reelFeedback = "PERFECT"
+
+    end
+
+    -- Normal reel / escape logic
     if reelSpeed >= self.safeMinReelSpeed
         and reelSpeed <= self.safeMaxReelSpeed
     then
+
         self.reelProgress += 100 / (2 * 30)
 
         self.escapeTimer -= 3 / 30
@@ -147,47 +206,68 @@ function FishingScene:fightingFishUpdate(
         if self.escapeTimer < 0 then
             self.escapeTimer = 0
         end
+
     else
+
         self.escapeTimer += 1 / 30
+
     end
 
     if self.escapeTimer >= self.escapeTime then
+
         self.fish:remove()
 
         self.reelProgress = 0
         self.escapeTimer = 0
+        self.reelFeedback = ""
 
         self.state = STATES.casting
 
         return
+
     end
 
     if self.reelProgress >= 100 then
+
         GAME_DATA.catchOfTheDay += 1
 
         self.fish:remove()
 
         self.reelProgress = 0
         self.escapeTimer = 0
+        self.reelFeedback = ""
 
         self.state = STATES.casting
+
     end
+
 end
 
 function FishingScene:drawOverlay()
+
     if self.state == STATES.reeling then
+
         self.rod:draw()
 
     elseif self.state == STATES.chargingCast then
+
         self:drawCastPower()
 
     elseif self.state == STATES.fightingFish then
+
         self.rod:draw()
+
         self:drawFishFight()
+
+        -- Reel speed feedback in center of screen
+        self:drawReelFeedback()
+
     end
+
 end
 
 function FishingScene:drawCastPower()
+
     local barX = 100
     local barY = 220
     local barWidth = 200
@@ -221,6 +301,42 @@ function FishingScene:drawCastPower()
         barWidth,
         barHeight
     )
+
+end
+
+function FishingScene:drawReelFeedback()
+
+    local text = self.reelFeedback
+
+    if text == "" then
+        return
+    end
+
+    local textWidth, textHeight =
+        gfx.getTextSize(text)
+
+    local x = 200 - textWidth / 2
+    local y = 120 - textHeight / 2
+
+    -- White background makes the message easier to see
+    gfx.setColor(gfx.kColorWhite)
+
+    gfx.fillRect(
+        x - 8,
+        y - 5,
+        textWidth + 16,
+        textHeight + 10
+    )
+
+    -- Black text
+    gfx.setColor(gfx.kColorBlack)
+
+    gfx.drawText(
+        text,
+        x,
+        y
+    )
+
 end
 
 function FishingScene:drawFishFight()
@@ -317,33 +433,45 @@ function FishingScene:drawFishFight()
 end
 
 function FishingScene:cleanup()
+
     if self.pierMenuItem then
+
         pd.getSystemMenu():removeMenuItem(
             self.pierMenuItem
         )
 
         self.pierMenuItem = nil
+
     end
 
     if self.player then
+
         self.player:remove()
         self.player = nil
+
     end
 
     if self.fish then
+
         self.fish:remove()
         self.fish = nil
+
     end
 
     if self.shore then
+
         self.shore:remove()
         self.shore = nil
+
     end
 
     if self.overlay then
+
         self.overlay:remove()
         self.overlay = nil
+
     end
 
     self:remove()
+
 end
